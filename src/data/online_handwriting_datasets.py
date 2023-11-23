8a6339a5dce105445ed731aaaa1f6f0a3864337e
Martin L <konten.ma.le@gmail.com>
2023-11-23 22:11:23 +0000

    Add draft of online_handwriting_datasets.OnlineHandwritingDataset class.

---
+++ src/data/online_handwriting_datasets.py
@@ -0,0 +1,11 @@
+class OnlineHandwritingDataset:
+
+    # TODO: Should be compatible with PyTorch dataset and/or
+    #       HuggingFace Dataset where I prefer the former.
+    #
+    #       The existing online handwriting datasets tend to be
+    #       relatively small so that they easily fit in RAM memory.
+    #
+    #       Caching them is useful nevertheless.
+
+    pass
