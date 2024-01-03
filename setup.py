b1a186b889f5ce40559890ee7823aee9b213aff5
Martin Lellep <starwars31337@gmail.com>
2024-01-03 19:17:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    description="Describe Your Cool Project",
-    author="",
-    author_email="",
-    url="https://github.com/user/project",
+    description="Implementation of 'Fast multi-language LSTM-based online handwriting recognition' paper by Carbune et al.",
+    author="Martin Lellep",
+    author_email="konten.ma.le@gmail.com",
+    url="https://github.com/PellelNitram/carbune2020_implementation",
     install_requires=["lightning", "hydra-core"],
     packages=find_packages(),
     # use this to customize global commands available in the terminal after installing the package
