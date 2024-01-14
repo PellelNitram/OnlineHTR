73f41c18587b20288f6290f2c4c097933139b18c
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-01-14 17:33:11 +0000

    Add OnlineHandwritingDataset.to_disc() method.

--- src/data/online_handwriting_datasets.py
+++ src/data/online_handwriting_datasets.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+import h5py
+
+
 class OnlineHandwritingDataset:
 
     # TODO: Should be compatible with PyTorch dataset and/or
@@ -20,13 +25,28 @@ class OnlineHandwritingDataset:
     # Methods that might be useful:
     # - some visualisation methods - plot image and also animated 2d and 3d video
     # - fit to bezier curve
-    # - (maybe) save to disk
 
     def load_data(self) -> None:
         """
         Needs to be implemented in subclasses.
         """
         raise NotImplementedError
+
+    def to_disc(self, path: Path) -> None:
+        """
+        Store OnlineHandwritingDataset to disc.
+
+        The OnlineHandwritingDataset is stored as HDF5 file of the structure:
+        - one group per sample
+        - one dataset per feature; those can be a time series as well as a single value
+
+        :param path: Path to save dataset to.
+        """
+        with h5py.File(path, 'w') as f:
+            for i, sample in enumerate( self.data ):
+                group = f.create_group(f'sample_{i}')
+                for key, value in sample.items():
+                    group.create_dataset(key, data=value)
 
 class IAMonDB_Dataset(OnlineHandwritingDataset):
 
