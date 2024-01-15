af70e2d5bb2b70d5680e9cb89ba145749ce8812a
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-01-15 11:20:55 +0000

    Add test_OnlineHandwritingDataset.test_set_data test.

--- tests/test_OnlineHandwritingDataset.py
+++ tests/test_OnlineHandwritingDataset.py
@@ -12,3 +12,19 @@ def test_construction():
     logger.setLevel(logging.INFO)
 
     ds = OnlineHandwritingDataset(logger=logger)
+
+def test_set_data():
+
+    logger = logging.getLogger('test_set_data')
+    logger.setLevel(logging.INFO)
+
+    ds = OnlineHandwritingDataset(logger=logger)
+
+    test_data = [ {'a': [ 0, 1 ], 'b': [ 2, 3 ]} ]
+
+    ds.set_data(test_data)
+
+    for d_ds, d_test in zip( ds.data, test_data ):
+        assert d_ds.keys() == d_test.keys()
+        for k in d_ds.keys():
+            assert d_ds[k] == d_test[k]
