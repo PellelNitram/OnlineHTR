dfd4643b53b1771526e5cb9feed0982ecac183ff
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-01-15 11:21:59 +0000

    Add test_OnlineHandwritingDataset.test_from_disc test.

--- tests/test_OnlineHandwritingDataset.py
+++ tests/test_OnlineHandwritingDataset.py
@@ -41,3 +41,26 @@ def test_to_disc(tmpdir):
     ds.set_data(test_data)
 
     ds.to_disc( tmpdir / 'test_to_disc.h5' )
+
+def test_from_disc(tmpdir):
+
+    logger = logging.getLogger('test_from_disc')
+    logger.setLevel(logging.INFO)
+
+    # First create and store a dataset to then load and compare against
+    ds = OnlineHandwritingDataset(logger=logger)
+    test_data = [ { 'a vector': [ 0, 1 ], 'one_number': 3.2, 'str': 'this is a test string' } ]
+    ds.set_data(test_data)
+    ds.to_disc( tmpdir / 'test_to_disc.h5' )
+
+    # Load dataset
+    ds_loaded = OnlineHandwritingDataset(logger=logger)
+    ds_loaded.from_disc( tmpdir / 'test_to_disc.h5' )
+
+    # Compare datasets
+    for s, s_loaded in zip( ds.data, ds_loaded.data ):
+        assert s.keys() == s_loaded.keys()
+        for key in s:
+            value = s[key]
+            value_loaded = s_loaded[key]
+            assert np.alltrue( value == value_loaded )
