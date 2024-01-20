3a9ef0938e6707b65114d01809774f7455e40c9e
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-01-20 17:37:52 +0000

    Add documents module.

---
+++ src/utils/documents.py
@@ -0,0 +1,5 @@
+"""
+This module provides infrastructure to interact with documents like Xournal files.
+
+This module was initially based on https://github.com/PellelNitram/xournalpp_htr/blob/dab111cab22891805d2eeaaececf2649eb70c58c/xournalpp_htr/documents.py.
+"""
