003a75845adef74c83e9b675f2f99b30cf73216e
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-01-26 20:24:01 +0000

    Work on test_XournalDocument test.

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -7,6 +7,8 @@ import pytest
 from src.utils import documents
 
 
+# Note: I don't test the dataclasses in excess to what I have done so already for Stroke class above.
+
 @pytest.mark.martin
 def test_Stroke_class():
 
@@ -36,21 +38,37 @@ def test_Stroke_class():
 @pytest.mark.martin
 def test_XournalDocument():
 
-    x_document = documents.XournalDocument(path=Path.home() / Path('data/code/carbune2020_implementation/data/datasets/2024-01-20-xournal_dataset.xoj'))
+    # TODO: Maybe add test of save_page_as_image method?
 
+    path = path=Path.home() / Path('data/code/carbune2020_implementation/data/datasets/2024-01-20-xournal_dataset.xoj')
+    x_document = documents.XournalDocument( path )
+
+    # Check document properties
+    assert x_document.path == path
+    assert x_document.DPI == 72
     assert len(x_document.pages) == 2
 
-    assert len( x_document.pages[0].layers[0].strokes ) == 0
+    # Check page properties
+    assert x_document.pages[1].background == {'type': 'solid', 'color': 'white', 'style': 'lined'}
+    assert x_document.pages[1].meta_data == {'width': '612.00', 'height': '792.00'}
+    assert len( x_document.pages[1].layers ) == 1
 
+    # Check layer properties
+    assert len( x_document.pages[0].layers[0].texts ) == 3
+    assert len( x_document.pages[1].layers[0].texts ) == 2
+    assert len( x_document.pages[0].layers[0].strokes ) == 0
+    assert len( x_document.pages[1].layers[0].strokes ) == 10
+
+    # Check stroke properties
+    assert x_document.pages[1].layers[0].strokes[0].meta_data == {
+        'tool': 'pen', 'color': 'black', 'width': '1.41'
+    }
+    assert len( x_document.pages[1].layers[0].strokes[0].x ) == 29
+    assert len( x_document.pages[1].layers[0].strokes[0].y ) == 29
+
+    # Check text properties
     assert x_document.pages[1].layers[0].texts[0].text == 'sample_name: hello_world'
     assert x_document.pages[1].layers[0].texts[1].text == 'label: Hello World!'
-    # TODO: Add some check about length of strokes
-
-    # TODO: Check all other parameters like background and pagea attributes; check load_data of XournalDocument for that. Also DPI.
-    # TODO: Maybe check save_page_as_image?
-    # TODO: Check path.
-
-    # TODO: note that I won't test the dataclasses in excess to what I have done so already for Stroke class above.
-
-    # TODO: Next: Implement and test XournalPagewiseDataset.
-    # TODO: Next: Build model and test on XournalPagewiseDataset!
+    assert x_document.pages[0].layers[0].texts[0].meta_data == {
+        'font': 'Sans', 'size': '12.00', 'x': '75.68', 'y': '88.34', 'color': 'black'
+    }
