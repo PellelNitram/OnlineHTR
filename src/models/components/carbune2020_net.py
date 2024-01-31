7ba24c63bf47be27d9862e9a2830584c354ad90a
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-01-31 18:00:52 +0000

    Add draft of Carbune2020Net class.

---
+++ src/models/components/carbune2020_net.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+
+
+class Carbune2020Net(nn.Module):
+    """TODO. Check SimpleDenseNet for inspiration."""
+
+    def __init__(
+        self,
+    ) -> None:
+        """TODO. Check SimpleDenseNet for inspiration."""
+        raise NotImplementedError # Check SimpleDenseNet for inspiration.
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """TODO. Check SimpleDenseNet for inspiration."""
+        raise NotImplementedError # Check SimpleDenseNet for inspiration.
