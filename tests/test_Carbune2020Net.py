e5e8e3d8bf4c3f3031061b2166bfc96e4938b3b1
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-02-05 16:29:56 +0000

    Fix test_construction test of test_Carbune2020Net.

--- tests/test_Carbune2020Net.py
+++ tests/test_Carbune2020Net.py
@@ -3,7 +3,7 @@ import logging
 
 import pytest
 
-from src.models.components.carbune2020_net import Carbune2020Net
+from src.models.components.carbune2020_net import Carbune2020NetAttempt1
 
 
 @pytest.mark.martin
@@ -12,7 +12,7 @@ def test_construction():
     logger = logging.getLogger('test_construction')
     logger.setLevel(logging.INFO)
 
-    net = Carbune2020Net(
+    net = Carbune2020NetAttempt1(
         number_of_channels=4,
         nodes_per_layer=64,
         number_of_layers=3,
