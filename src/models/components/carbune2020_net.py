bdb8da8d2d47d2388115eb321f4b61bade0595a2
Martin Lellep <konten.ma.le@gmail.com>
2024-02-07 21:04:58 +0000

    Work on Carbune2020NetAttempt1.forward method.

--- src/models/components/carbune2020_net.py
+++ src/models/components/carbune2020_net.py
@@ -55,3 +55,7 @@ class Carbune2020NetAttempt1(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """TODO. Check SimpleDenseNet for inspiration."""
         raise NotImplementedError # Check SimpleDenseNet for inspiration.
+        result = self.lstm_stack(x)
+        result = self.linear(result)
+        result = self.log_softmax(result)
+        return result
