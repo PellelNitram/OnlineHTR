6491b609e6c1fd445f3bc7b18dac9e1ec4b06c59
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-02-14 17:46:20 +0000

    Work on train_no_hydra script.

--- src/train_no_hydra.py
+++ src/train_no_hydra.py
@@ -6,10 +6,16 @@
 import lightning as L
 from lightning import Callback, LightningDataModule, LightningModule, Trainer
 import torch
+from torch.optim import Adam
+from lightning.pytorch.trainer import Trainer
 
 from src.utils import (
     RankedLogger,
 )
+from src.data.online_handwriting_datasets import XournalPagewiseDatasetPyTorch
+from src.data.online_handwriting_datamodule import SimpleOnlineHandwritingDataModule
+from src.models.carbune_module import CarbuneLitModule
+from src.models.components.carbune2020_net import Carbune2020NetAttempt1
 
 # ==============================================
 # ================== Settings ==================
@@ -25,19 +31,79 @@ log = RankedLogger(__name__, rank_zero_only=True)
 
 L.seed_everything(SEED, workers=True)
 
+path = '/storage/datastore-personal/s1691089/data/code/carbune2020_implementation/data/datasets/2024-01-20-xournal_dataset.xoj'
+output_dir = '/storage/datastore-personal/s1691089/data/code/carbune2020_implementation/data/train_no_hydra'
+
+log.info(f"Wee test: XournalPagewiseDatasetPyTorch can be initialised")
+ds = XournalPagewiseDatasetPyTorch(
+    path,
+)
+# Seems to work - good!
+# -> This can become a test later.
+# del ds
+print( ds[0] )
+# ALPHABET HACK
+alphabet = list( set( ds[0]['label'].lower() ))
+alphabet = sorted( alphabet )
+
 log.info(f"Instantiating datamodule")
-datamodule: LightningDataModule = # TODO
+datamodule: LightningDataModule = SimpleOnlineHandwritingDataModule(
+    path, (1, 0, 0), 64, 0, False,
+)
 
 log.info(f"Instantiating model")
-model: LightningModule = # TODO
 
-log.info("Instantiating callbacks...")
-callbacks: List[Callback] = # TODO
 
-log.info("Instantiating loggers...")
-logger: List[Logger] = # TODO
+# model: LightningModule = # TODO
+net = Carbune2020NetAttempt1(
+    3,
+    nodes_per_layer=64,
+    number_of_layers=3,
+    dropout=0.0,
+    alphabet=alphabet,
+)
+# optimiser = Adam(
+#     # TODO: missing params,
+#     lr=0.001,
+#     weight_decay=0.0,
+# )
+# scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+#     mode='min',
+#     factor=0.1,
+#     patience=10, 
+# )
+model: LightningModule = CarbuneLitModule(
+    net,
+    optimizer=None,
+    scheduler=None,
+    compile=False,
+)
+
+# log.info("Instantiating callbacks...")
+# callbacks: List[Callback] = # TODO
+
+# log.info("Instantiating loggers...")
+# logger: List[Logger] = # TODO
 
 log.info(f"Instantiating trainer")
-trainer: Trainer = # TODO
+trainer: Trainer = Trainer(
+    default_root_dir=output_dir,
+    min_epochs=1, # prevents early stopping
+    max_epochs=10,
+    accelerator='gpu',
+    devices=1,
+    check_val_every_n_epoch=1,
+    deterministic=False,
+    # Added back in later:
+    # callbacks=callbacks,
+    # logger=logger,
+)
 
-# TODO
+raise NotImplementedError('Implement transformations!')
+
+log.info("Starting training!")
+trainer.fit(
+    model=model,
+    datamodule=datamodule,
+    # ckpt_path=cfg.get("ckpt_path"),
+)
