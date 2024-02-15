edb526e81745de51b27006b34a5de4db4c1c25d6
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-02-15 22:56:37 +0000

    Add transforms.TwoChannels class.

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -11,3 +11,34 @@
 # - same as above but with transforms like differences, differences after equidistance transform, Bezier curves
 # - left alone vs lowered label alphabet
 # - TODO: What to code as transform vs what to code as transform?
+
+import torch
+import numpy as np
+
+
+class TwoChannels(object):
+    """TODO.
+
+    Return { ink: (x, y), label: label } where label is list of letters.
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, sample):
+        """TODO.
+
+        :returns: { ink, label }
+        """
+
+        x = sample['x']
+        y = sample['y']
+
+        ink = np.vstack([x, y]).T
+
+        label = sample['label']
+
+        return {
+            'ink': torch.from_numpy(ink),
+            'label': label,
+        }
