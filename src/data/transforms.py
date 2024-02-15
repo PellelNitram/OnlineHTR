881f2786d6e1bb4f7473846106f3cee99eda075d
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-02-15 22:55:35 +0000

    Work on transforms module.

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -4,5 +4,10 @@
 
 # Transforms that I need for sure:
 # - As input a stack of (x, y, stroke_nr) and as output label.
+# - Switch axis to fit axis of what model reads as input.
 # - same as above but with t
+# - label to alphabet
+# - lower text in label
 # - same as above but with transforms like differences, differences after equidistance transform, Bezier curves
+# - left alone vs lowered label alphabet
+# - TODO: What to code as transform vs what to code as transform?
