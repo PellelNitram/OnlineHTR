b32e17788c4bb2628680609cabee06e7658e170a
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-02-29 14:40:03 +0000

    Add draft of decoders.GreedyCTCDecoder.

---
+++ src/utils/decoders.py
@@ -0,0 +1,27 @@
+import torch
+
+
+class GreedyCTCDecoder(torch.nn.Module):
+    """TODO.
+
+    Loosely based on [this](https://pytorch.org/audio/main/tutorials/asr_inference_with_ctc_decoder_tutorial.html#greedy-decoder).
+    """
+
+    def __init__(self, labels, blank=0):
+        super().__init__()
+        self.labels = labels
+        self.blank = blank
+
+    def forward(self, emission: torch.Tensor) -> List[str]:
+        """Given a sequence emission over labels, get the best path
+        Args:
+          emission (Tensor): Logit tensors. Shape `[num_seq, num_label]`.
+
+        Returns:
+          List[str]: The resulting transcript
+        """
+        indices = torch.argmax(emission, dim=-1)  # [num_seq,]
+        indices = torch.unique_consecutive(indices, dim=-1)
+        indices = [i for i in indices if i != self.blank]
+        joined = "".join([self.labels[i] for i in indices])
+        return joined.replace("|", " ").strip().split()
