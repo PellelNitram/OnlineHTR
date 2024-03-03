6b5bd712404e441de38c7873c2179dbfe3c32428
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-03 13:36:55 +0000

    Add io.load_IAM_OnDB_sample function.

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from pathlib import Path
 
 import pandas as pd
 
@@ -58,3 +59,29 @@ def load_IAM_OnDB_text_line(path, line_nr):
     all_lines = [ xx for xx in all_lines if len(xx) > 0 ]
 
     return all_lines[line_nr]
+
+def load_IAM_OnDB_sample(sample, base_path):
+    """
+    Load IAM On-DB data sample.
+
+    With sample consisting of time series and text line as ground truth.
+
+    :param sample: Sample code according to IAM On-DB encoding.
+    :param base_path: Base path of IAM On-DB.
+    :returns: (df, text_line) with df as time series.
+    """
+
+    SPLITTER = '-'
+
+    code1, code2, code3 = sample.split(SPLITTER)
+    code2_no_letters = ''.join( [ letter for letter in code2 if letter in '0123456789' ] )
+
+    strokes_file = Path( base_path / f'lineStrokes-all/{code1}/{code1}{SPLITTER}{code2_no_letters}/{code1}{SPLITTER}{code2}{SPLITTER}{code3}.xml' )
+    text_line_file = Path( base_path / f'ascii-all/{code1}/{code1}{SPLITTER}{code2_no_letters}/{code1}{SPLITTER}{code2}.txt' )
+
+    df = load_df_iam_ondb(strokes_file)
+    df['y'] *= -1 # Correct text direction to natural direction facing upwards
+
+    text_line = load_IAM_OnDB_text_line(text_line_file, int(code3)-1)
+
+    return df, text_line
