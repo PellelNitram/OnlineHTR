9ea2635f2c438ae7d762975db7bd306685ff995b
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-05 15:27:33 +0000

    Add test_IAM_OnDB_Dataset.test_correctness_manually test.

--- tests/test_IAM_OnDB_Dataset.py
+++ tests/test_IAM_OnDB_Dataset.py
@@ -26,3 +26,29 @@ def test_construction_no_limit():
     length = 12187 # Determined empirically
 
     assert len(ds) == length
+
+@pytest.mark.martin
+@pytest.mark.slow
+def test_correctness_manually(tmp_path: Path):
+    # This saves samples to files so that one can inspect the correctness of the
+    # dataset manually. Enabling the pytest setting `-s` allows one to see where
+    # the files were saved temporarily.
+
+    NR_SAMPLES = 100
+    LIMIT = -1
+
+    print()
+    print(f'Samples saved at: "{tmp_path}"')
+    print()
+
+    ds = IAM_OnDB_Dataset(path=PATH, transform=None, limit=LIMIT)
+
+    # Get NR_SAMPLES reproducible random draws
+    rng = np.random.default_rng(1337)
+    index_list = np.arange(0, len(ds))
+    rng.shuffle(index_list)
+    index_list = index_list[:NR_SAMPLES]
+
+    for iam_index in index_list:
+        sample_name = ds[iam_index]['sample_name']
+        ds.plot_sample_to_image_file(iam_index, tmp_path / Path(f'{sample_name}.png'))
