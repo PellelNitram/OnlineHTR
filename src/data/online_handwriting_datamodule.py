f05b873ef40da6185eb9e92997e4b1826bd388fe
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-07 21:59:25 +0000

    Work on IAMOnDBDataModule.

--- src/data/online_handwriting_datamodule.py
+++ src/data/online_handwriting_datamodule.py
@@ -12,6 +12,10 @@ from src.data.online_handwriting_datasets import IAM_OnDB_Dataset
 from src.data.transforms import TwoChannels
 from src.data.transforms import CharactersToIndices
 from src.data.collate_functions import my_collator
+from src.data.transforms import TwoChannels
+from src.data.tokenisers import AlphabetMapper
+from src.data.online_handwriting_datasets import get_alphabet_from_dataset
+from src.data.online_handwriting_datasets import get_number_of_channels_from_dataset
 
 
 class SimpleOnlineHandwritingDataModule(LightningDataModule):
@@ -164,3 +168,120 @@ class SimpleOnlineHandwritingDataModule(LightningDataModule):
             Defaults to ``None``.
         """
         pass
+
+class IAMOnDBDataModule(LightningDataModule):
+    """TODO."""
+
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
+        batch_size: int = 64,
+        num_workers: int = 0,
+        limit: int = -1,
+        pin_memory: bool = False,
+    ) -> None:
+        """Initialize a `MNISTDataModule`.
+
+        TODO.
+
+        :param data_dir: The data directory. Defaults to `"data/"`.
+        :param train_val_test_split: The train, validation and test split. Defaults to `(55_000, 5_000, 10_000)`.
+        :param batch_size: The batch size. Defaults to `64`.
+        :param num_workers: The number of workers. Defaults to `0`.
+        :param pin_memory: Whether to pin memory. Defaults to `False`.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+        self.batch_size_per_device = batch_size
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Load data.
+        
+        Set variables `self.data_train`, `self.data_val` and `self.data_test`.
+
+        :param stage: The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`. Defaults to ``None``.
+        """
+
+        if not self.data_train and not self.data_val and not self.data_test:
+
+            if sum(self.hparams.train_val_test_split) > len(dataset):
+                raise RuntimeError(
+                    f"Dataset (len={len(dataset)}) too short for requested splits ({self.hparams.train_val_test_split})."
+                )
+
+            dataset = IAM_OnDB_Dataset(
+                    Path(self.hparams.data_dir),
+                    transform=None,
+                    limit=self.hparams.limit,
+            )
+
+            self.alphabet = get_alphabet_from_dataset( dataset )
+            self.alphabet_mapper = AlphabetMapper( self.alphabet )
+
+            self.number_of_channels = get_number_of_channels_from_dataset( dataset )
+
+            # TODO: Add transforms as parameter that are then used in setup. So that they
+            #       can be parameterised w/ Hydra later on.
+            transform = transforms.Compose([
+                TwoChannels(),
+                CharactersToIndices( self.alphabet ),
+            ])
+
+            dataset.transform = transform
+            
+            self.data_train, self.data_val, self.data_test = random_split(
+                dataset=dataset,
+                lengths=self.hparams.train_val_test_split,
+                generator=torch.Generator().manual_seed(42),
+            )
+            
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader.
+
+        :return: The train dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+            collate_fn=my_collator,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader.
+
+        :return: The validation dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+            collate_fn=my_collator,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader.
+
+        :return: The test dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+            collate_fn=my_collator,
+        )
