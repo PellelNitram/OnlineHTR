94dab1c82be9095dec3c3fd38236ebd614c13395
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-09 09:38:38 +0000

    Add test_CharactersToIndices.test_call.

--- tests/test_CharactersToIndices.py
+++ tests/test_CharactersToIndices.py
@@ -12,3 +12,39 @@ def test_instantiate():
     ]
 
     transform = CharactersToIndices(alphabet)
+
+def test_call():
+
+    alphabet = [
+        'h',
+        'e',
+        'l',
+        'o',
+    ]
+
+    sample = {
+        'label': 'hello',
+        'channel1': 'abcdef',
+        'channel2': [
+            1,
+            2,
+            4,
+            3,
+            5,
+        ],
+    }
+
+    transform = CharactersToIndices(alphabet)
+
+    sample_transformed = transform(sample)
+
+    # Check that other channels remain the same
+    assert    sample['channel1']  ==    sample_transformed['channel1']
+    assert id(sample['channel1']) == id(sample_transformed['channel1'])
+
+    assert    sample['channel2']  ==    sample_transformed['channel2']
+    assert id(sample['channel2']) == id(sample_transformed['channel2'])
+
+    # Check that label is correct
+    assert torch.equal( sample['label'], torch.as_tensor([1, 2, 3, 3, 4], dtype=torch.int64) )
+    assert sample['label'].dtype == torch.int64
