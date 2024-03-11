0038e7df5d1feb43333ba154c86a1ed13bf352ab
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-11 10:51:55 +0000

    Update experiment1 bash script.

--- scripts/experiment1.sh
+++ scripts/experiment1.sh
@@ -1,1 +1,5 @@
-python src/train.py --config-name experiment1.yaml
+python src/train.py \
+    --config-name experiment1.yaml \
+    -m \
+    trainer.max_epochs=10000,20000,30000 \
+    trainer.accelerator=gpu,cpu
