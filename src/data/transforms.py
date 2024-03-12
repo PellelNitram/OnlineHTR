27f3e695865f0797a6206c7d91f83029b9d52be4
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-12 13:58:01 +0000

    Start to work on Carbune2020 transform.

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -14,6 +14,7 @@
 
 import torch
 import numpy as np
+import pandas as pd
 
 from src.data.tokenisers import AlphabetMapper
 
@@ -72,3 +73,55 @@ class CharactersToIndices(object):
         sample['label'] = label # Updated in-place
 
         return sample
+
+class Carbune2020(object):
+    """TODO.
+
+    Returns TODO.
+    - `label` key is copied over from sample w/o change.
+    - `ink` is computed according to [Carbune2020].
+    """
+
+    POINTS_PER_UNIT_LENGTH = 20.0
+
+    def __init__(self):
+        pass
+
+    def __call__(self, sample):
+        """TODO.
+
+        The sample is changed in-place.
+
+        :returns: TODO.
+        """
+
+        pass
+
+
+        sample_sub = {
+            'x': sample['x'],
+            'y': sample['y'],
+            't': sample['t'],
+            'stroke_nr': sample['stroke_nr'],
+        }
+        df = pd.DataFrame.from_dict(sample_sub)
+
+        df['x'] = df['x'] - df['x'].iloc[0]
+        df['y'] = df['y'] - df['y'].min()
+        scale_factor = df['y'].max()
+        df['x'] = df['x'] / scale_factor
+        df['y'] = df['y'] / scale_factor
+        if df['x'].iloc[0] != 0:
+            raise ValueError('x shift failed')
+        if df['y'].min() != 0.0 or df['y'].max() != 1.0:
+            raise ValueError('y shift and rescaling failed')
+
+        print(df)
+        raise NotImplementedError
+
+        ink = None # TODO: fill it
+
+        return {
+            'label': sample['label'],
+            'ink': ink,
+        }
