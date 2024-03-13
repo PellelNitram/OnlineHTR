037e429bca748858082d9c5b1dc4a7dbeba52dce
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-13 13:50:40 +0000

    Add experiment3 bash script.

---
+++ scripts/experiment3.sh
@@ -0,0 +1,9 @@
+python src/train.py \
+    --config-name experiment2.yaml \
+    -m \
+    trainer.max_epochs=1000 \
+    data.limit=-1 \
+    data.train_val_test_split="[1.0,0,0]" \
+    model.optimizer.lr=0.001,0.0001
+
+# I just reuse the experiment2 config for easier experiment starting here.
