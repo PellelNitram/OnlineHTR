39473dc56f76fade1ce793982d50b93d69716758
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-16 21:49:04 +0000

    Update experiment3 bash script.

--- scripts/experiment3.sh
+++ scripts/experiment3.sh
@@ -15,4 +15,22 @@ python src/train.py \
     data.train_val_test_split="[1.0,0,0]" \
     model.optimizer.lr=0.001,0.0001
 
+python src/train.py \
+    --config-name experiment2.yaml \
+    -m \
+    data.batch_size=64 \
+    trainer.max_epochs=3000 \
+    data.limit=-1 \
+    data.train_val_test_split="[1.0,0,0]" \
+    model.optimizer.lr=0.0001
+
+python src/train.py \
+    --config-name experiment2.yaml \
+    -m \
+    data.batch_size=128 \
+    trainer.max_epochs=3000 \
+    data.limit=-1 \
+    data.train_val_test_split="[1.0,0,0]" \
+    model.optimizer.lr=0.001
+
 # I just reuse the experiment2 config for easier experiment starting here.
