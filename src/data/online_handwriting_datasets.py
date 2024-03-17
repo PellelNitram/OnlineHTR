a01185b782b7ca70387618858d4de5b407c648b3
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-17 16:30:16 +0000

    Add skip_carbune2020_fails feature to IAM_OnDB_Dataset.

--- src/data/online_handwriting_datasets.py
+++ src/data/online_handwriting_datasets.py
@@ -335,17 +335,82 @@ class IAM_OnDB_Dataset(Dataset):
         'z01-000z-08', # There exists no text for that sample
     ]
 
-    def __init__(self, path: Path, transform=None, limit: int=-1) -> None:
+    # These are the samples that fail when transformed with Carbune2020 transformation.
+    # I determined the samples here empirically.
+    SAMPLES_TO_SKIP_BC_CARBUNE2020_FAILS = [
+        'c02-082-06',
+        'c02-082-02',
+        'p04-468z-01',
+        'e04-026-01',
+        'e04-083-06',
+        'e04-083-03',
+        'e04-083-02',
+        'a01-007w-01',
+        'a01-087-02',
+        'a01-020x-03',
+        'a01-020x-04',
+        'a01-053-03',
+        'a01-053-04',
+        'a01-053x-01',
+        'a01-053x-03',
+        'p09-110z-06',
+        'd04-125-05',
+        'h01-030-03',
+        'a02-037-04',
+        'a02-102-02',
+        'a02-017-05',
+        'a02-120-03',
+        'k08-835z-01',
+        'c08-465z-07',
+        'c04-134-01',
+        'c04-061-01',
+        'g01-004-05',
+        'h02-037-02',
+        'h02-037-03',
+        'h02-024-04',
+        'j08-408z-05',
+        'b04-134-04',
+        'g06-000n-02',
+        'g06-000k-03',
+        'g06-000i-06',
+        'g06-000k-01',
+        'g06-000i-09',
+        'g06-000f-04',
+        'h07-260z-05',
+        'h07-013-02',
+        'm05-480z-01',
+        'm05-538z-05',
+        'a04-047-03',
+        'b05-032-02',
+        'b05-032-03',
+        'g07-065-02',
+        'g03-026-03',
+        'd01-024-02',
+        'f07-028b-02',
+        'f07-028b-05',
+        'f03-222z-03',
+        'f03-174-04',
+        'j04-015-02',
+        'g04-055-02',
+        'n01-051z-05',
+        'l06-644z-02',
+        'g10-343z-07',
+        'a06-070-03',
+    ]
+
+    def __init__(self, path: Path, transform=None, limit: int=-1, skip_carbune2020_fails: bool=False) -> None:
         """
         TODO: Explain how the dataset needs to be stored on disk to allow access
         to it using this present class.
 
         :param path: Path to dataset.
         :param limit: Limit number of loaded samples to this value if positive.
+        :param skip_carbune2020_fails: Skip all sample that are known to fail when the `Carbune2020` transform is applied.
         """
         self.path = path
         self.transform = transform
         self.limit = limit
+        self.skip_carbune2020_fails = skip_carbune2020_fails
         self.data = self.load_data()
         # TODO: I'd love to add logging here to understand the skipped images
 
@@ -381,6 +446,9 @@ class IAM_OnDB_Dataset(Dataset):
                         break
 
                     if sample_name in self.SAMPLES_NOT_TO_STORE:
+                        continue
+
+                    if self.skip_carbune2020_fails and sample_name in self.SAMPLES_TO_SKIP_BC_CARBUNE2020_FAILS:
                         continue
 
                     df, text_line = load_IAM_OnDB_sample(sample_name, self.path)
