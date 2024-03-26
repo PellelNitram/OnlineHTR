fd2e6ebd305555710b3f67ca420894d443a4eb95
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-26 19:30:58 +0000

    Update experiment7 bash script.

--- scripts/experiment7.sh
+++ scripts/experiment7.sh
@@ -1,6 +1,7 @@
 # Perform experiment5 and experiment6 for fewer epochs
 # but with validation enabled.
 
+# No dropout
 time python src/train.py \
     --config-name experiment2.yaml \
     -m \
@@ -12,4 +13,19 @@ time python src/train.py \
     data.pin_memory=True \
     data.num_workers=4 \
     data.transform="carbune2020_xytn","carbune2020_xyn" \
+    tags="experiment7"
+
+# With dropout
+time python src/train.py \
+    --config-name experiment2.yaml \
+    -m \
+    data.batch_size=64 \
+    trainer.max_epochs=2000 \
+    data.limit=-1 \
+    data.train_val_test_split="[0.8,0.2,0]" \
+    model.optimizer.lr=0.0001 \
+    data.pin_memory=True \
+    data.num_workers=4 \
+    data.transform="carbune2020_xytn","carbune2020_xyn" \
+    model.net.dropout=0.25 \
     tags="experiment7"
