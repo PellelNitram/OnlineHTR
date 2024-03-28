fba5b7625913897b072b9ae2f753602d6e606097
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-28 11:34:47 +0000

    Update experiment8 bash script for production run.

--- scripts/experiment8.sh
+++ scripts/experiment8.sh
@@ -4,12 +4,13 @@ time python src/train.py \
     --config-name experiment_w_callbacks.yaml \
     -m \
     data.batch_size=64 \
-    trainer.max_epochs=30 \
-    data.limit=200 \
+    trainer.max_epochs=2000 \
+    trainer.check_val_every_n_epoch=50 \
+    data.limit=-1 \
     data.train_val_test_split="[0.8,0.2,0]" \
     model.optimizer.lr=0.0001 \
     data.pin_memory=True \
     data.num_workers=4 \
     data.transform="carbune2020_xyn" \
-    callbacks.model_checkpoint.every_n_epochs=10 \
+    callbacks.model_checkpoint.every_n_epochs=50 \
     tags="experiment8"
