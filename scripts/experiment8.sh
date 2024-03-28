06249098e450709390dcc7c806a2f825a928a9ec
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-28 09:54:10 +0000

    Add experiment8 bash script.

---
+++ scripts/experiment8.sh
@@ -0,0 +1,14 @@
+# Perform training w/ callbacks to use them for inference subsequently
+
+time python src/train.py \
+    --config-name experiment_w_callbacks.yaml \
+    -m \
+    data.batch_size=64 \
+    trainer.max_epochs=30 \
+    data.limit=200 \
+    data.train_val_test_split="[0.8,0.2,0]" \
+    model.optimizer.lr=0.0001 \
+    data.pin_memory=True \
+    data.num_workers=4 \
+    data.transform="carbune2020_xyn" \
+    tags="experiment8"
