62cbe2ab2fefe03b360090475e03405b6b89c922
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-03-31 23:39:20 +0100

    Fix src.utils.callbacks.TrainEpochTimeMeasuring callback.

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -3,18 +3,21 @@ import time
 from lightning.pytorch.callbacks import Callback
 
 
-class TrainEpochTimeMeasuring(L.Callback):
+class TrainEpochTimeMeasuring(Callback):
     """Measures train epoch compute time."""
 
     def __init__(self):
         self.time_cache = None
 
-    def on_train_epoch_start(self, trainer, pl_module):
+    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
         self.time_cache = time.perf_counter()
 
-    def on_train_epoch_end(self, trainer, pl_module):
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         time_difference = time.perf_counter() - self.time_cache
-        pl_module.log('epoch_time', time_difference, pl_module.current_epoch)
-        # TODO: Potential problem are validation steps as they are part of the training loop.
-        #       Should I maybe instead track batch times instead as they do not include the validation loop.
-        #       See here: https://lightning.ai/docs/pytorch/stable/common/lightning_module.html#hooks.
+        pl_module.log(
+            'batch_time',
+            time_difference,
+            on_step=True,
+            on_epoch=False,
+            prog_bar=False
+        )
