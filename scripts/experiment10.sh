087bd381a87e20258b6273274b6e081244d56d43
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-04-04 23:06:10 +0100

    Add experiment10 bash script.

---
+++ scripts/experiment10.sh
@@ -0,0 +1,10 @@
+# Run LitModule1 to test if I can easily load it
+
+time python src/train.py \
+    --config-name experiment_LitModule1.yaml \
+    -m \
+    trainer.max_epochs=5000 \
+    data.limit=-1 \
+    data.train_val_test_split="[0.8,0.2,0]" \
+    data.transform="carbune2020_xyn" \
+    tags="experiment10"
