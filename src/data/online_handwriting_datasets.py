4cfbc76e5913d0662ba47d0afc6405ded3ba44af
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-04-10 09:03:21 +0100

    Add TODO comment to IAM_OnDB_Dataset class.

--- src/data/online_handwriting_datasets.py
+++ src/data/online_handwriting_datasets.py
@@ -358,6 +358,8 @@ class IAM_OnDB_Dataset(Dataset):
     This is the raw dataset which can be further processed using downstream transformations.
     """
 
+    # TODO: Add progress bar to this class!
+
     LENGTH = 12187 # Determined empirically
 
     SAMPLES_NOT_TO_STORE = [
