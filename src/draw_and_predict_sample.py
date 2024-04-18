f018d89bbaa593146563cd75c4f608aebe595363
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-04-18 11:53:40 +0100

    Major restructure of draw_and_predict_sample script.
    - Add __name__ guard.
    - Add main function.
    - Add command-line argument parsing function.

--- src/draw_and_predict_sample.py
+++ src/draw_and_predict_sample.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from pathlib import Path
+import argparse
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,60 +19,73 @@ from src.data.acquisition import Sketchpad
 from src.data.acquisition import predict
 
 
-# ========
-# Settings
-# ========
+def parse_cli_args():
+    """Parse command-line arguments for this script."""
 
-PATH = 'logs/train/multiruns/2024-04-10_20-09-25/8'
-DOT_RADIUS = 1
-DOT_RADIUS = 3
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dot-radius', '--dot-radius', type=int,
+                        default=3)
+    parser.add_argument('-base-path', '--base-path', type=Path,
+                        default=Path('logs/train/multiruns/2024-04-10_20-09-25/8'))
+    args = parser.parse_args()
+    
+    return vars(args)
 
-# =====
-# Model
-# =====
+def main(args):
 
-BASE_PATH = Path(PATH)
-CHECKPOINT_PATH = BASE_PATH / 'checkpoints/epoch000749.ckpt'
+    # =====
+    # Model
+    # =====
 
-model = LitModule1.load_from_checkpoint(CHECKPOINT_PATH)
+    BASE_PATH = Path(args['base_path'])
+    CHECKPOINT_PATH = BASE_PATH / 'checkpoints/epoch000749.ckpt'
 
-model.eval()
+    model = LitModule1.load_from_checkpoint(CHECKPOINT_PATH)
 
-checkpoint = torch.load(CHECKPOINT_PATH, map_location=lambda storage, loc: storage)
+    model.eval()
 
-alphabet = load_alphabet(BASE_PATH / 'alphabet.json')
-alphabet_mapper = AlphabetMapper( alphabet )
-decoder = checkpoint['hyper_parameters']['decoder']
+    checkpoint = torch.load(CHECKPOINT_PATH, map_location=lambda storage, loc: storage)
 
-# ==
-# UI
-# ==
+    alphabet = load_alphabet(BASE_PATH / 'alphabet.json')
+    alphabet_mapper = AlphabetMapper( alphabet )
+    decoder = checkpoint['hyper_parameters']['decoder']
 
-global_strokes = []
+    # ==
+    # UI
+    # ==
 
-root = Tk()
-root.title("Draw and Predict Sample")
-root.geometry("1024x512")
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+    global_strokes = []
 
-sketch = Sketchpad(root, global_strokes, DOT_RADIUS)
-sketch.grid(column=0, row=0, sticky=(N, W, E, S))
+    root = Tk()
+    root.title("Draw and Predict Sample")
+    root.geometry("1024x512")
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
 
-prediction_field = Text(root, height=5, width=100)
-prediction_field.place(x=300, y=400)
-prediction_field.tag_configure('big', font=('Arial', 20, 'bold', 'italic'))
+    sketch = Sketchpad(root, global_strokes, args['dot_radius'])
+    sketch.grid(column=0, row=0, sticky=(N, W, E, S))
 
-plot_button = Button(root, text="Plot strokes", command=lambda: plot_strokes(global_strokes))
-plot_button.place(x=50,y=50)
+    prediction_field = Text(root, height=5, width=100)
+    prediction_field.place(x=300, y=400)
+    prediction_field.tag_configure('big', font=('Arial', 20, 'bold', 'italic'))
 
-store_button = Button(root, text="Store strokes", command=lambda: store_strokes(global_strokes))
-store_button.place(x=200,y=50)
+    plot_button = Button(root, text="Plot strokes", command=lambda: plot_strokes(global_strokes))
+    plot_button.place(x=50,y=50)
 
-reset_button = Button(root, text="Reset strokes", command=lambda: reset_strokes(global_strokes, sketch, prediction_field))
-reset_button.place(x=800,y=50)
+    store_button = Button(root, text="Store strokes", command=lambda: store_strokes(global_strokes))
+    store_button.place(x=200,y=50)
 
-predict_button = Button(root, text="Predict!", command=lambda: predict(global_strokes, prediction_field, alphabet, model, decoder, alphabet_mapper))
-predict_button.place(x=50,y=400)
+    reset_button = Button(root, text="Reset strokes", command=lambda: reset_strokes(global_strokes, sketch, prediction_field))
+    reset_button.place(x=800,y=50)
 
-root.mainloop()
+    predict_button = Button(root, text="Predict!", command=lambda: predict(global_strokes, prediction_field, alphabet, model, decoder, alphabet_mapper))
+    predict_button.place(x=50,y=400)
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+
+    args = parse_cli_args()
+
+    main(args)
