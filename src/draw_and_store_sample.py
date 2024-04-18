2f1910c063e90c170d14a562da9bccdd7143c37d
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-04-18 22:01:32 +0100

    Major restructure of draw_and_store_sample script.
     - Add __name__ guard.
     - Add main function.
     - Add command-line argument parsing function.

--- src/draw_and_store_sample.py
+++ src/draw_and_store_sample.py
@@ -1,32 +1,51 @@
-from tkinter import *
-from tkinter import ttk
-from time import time
-from tkinter import filedialog
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+from tkinter import Tk
+from tkinter import Button
+from tkinter import N, W, E, S
+import argparse
 
 from src.data.acquisition import Sketchpad
 from src.data.acquisition import plot_strokes
 from src.data.acquisition import store_strokes
 
 
-global_strokes = []
+def parse_cli_args() -> dict:
+    """Parse command-line arguments for this script."""
 
-root = Tk()
-root.geometry("1024x512")
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dot-radius', '--dot-radius', type=int,
+                        default=3)
+    args = parser.parse_args()
+    
+    return vars(args)
 
-DOT_RADIUS = 3
-sketch = Sketchpad(root, global_strokes, DOT_RADIUS)
-sketch.grid(column=0, row=0, sticky=(N, W, E, S))
+def main(args: dict) -> None:
+    """Main function of this script.
 
-plot_button = Button(root, text="Plot strokes", command=lambda: plot_strokes(global_strokes))
-plot_button.place(x=50,y=50)
+    :param args: Arguments to this main function.
+    """
 
-store_button = Button(root, text="Store strokes", command=lambda: store_strokes(global_strokes))
-store_button.place(x=200,y=50)
+    global_strokes = []
 
-root.mainloop()
+    root = Tk()
+    root.geometry("1024x512")
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
+    sketch = Sketchpad(root, global_strokes, args['dot_radius'])
+    sketch.grid(column=0, row=0, sticky=(N, W, E, S))
+
+    plot_button = Button(root, text="Plot strokes",
+                         command=lambda: plot_strokes(global_strokes))
+    plot_button.place(x=50,y=50)
+
+    store_button = Button(root, text="Store strokes",
+                          command=lambda: store_strokes(global_strokes))
+    store_button.place(x=200,y=50)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+
+    args = parse_cli_args()
+
+    main(args)
