80163a13213c909027a37c396b26f801ba14465b
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-04-23 23:09:00 +0100

    Update experiment13 bash script.

--- scripts/experiment13.sh
+++ scripts/experiment13.sh
@@ -1,10 +1,13 @@
-# Run speed test to see if avoiding re-computing string labels is faster
+# 1. Run speed test to see if avoiding re-computing string labels is faster
+#   - Old version to run this script at: a18972f197f8ffae396874b4d7e184919d40b229
+#   - New version to run this script at: 5a056f10c6009de5a34440e4da80a24c9720886a
+# 2. Run speed test to see if removing text decoding, WER/CER computations and logging
+#    thereof is faster
+#   - Old version to run this script at: b464132e91e4f6ac644954347773b7e60754c6e8
+#   - New version to run this script at: e1337475baea1e937237c6d5f4b6b59343e3cf10
 
-# Old version to run this script at: a18972f197f8ffae396874b4d7e184919d40b229
-# New version to run this script at: 5a056f10c6009de5a34440e4da80a24c9720886a
-
-# I run those two experiments using this script here at the two git commits
-# manually as it's easier.
+# I run those two speed tests using this script here at the two corresponding old and
+# new git commits manually.
 
 time python src/train.py \
     --config-name experiment_LitModule1.yaml \
