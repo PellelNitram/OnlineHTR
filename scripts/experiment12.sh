a43b51a3a9a2c34af36f8ee9789966ba5463c2d2
Martin L <konten.ma.le@gmail.com>
2024-04-25 16:12:22 +0000

    Update experiment12 bash script.

--- scripts/experiment12.sh
+++ scripts/experiment12.sh
@@ -16,3 +16,19 @@ time python src/train.py \
     tags="experiment12" \
     experiment_name="experiment12" \
     trial_name="trial1"
+
+time python src/train.py \
+    --config-name experiment_LitModule1.yaml \
+    -m \
+    trainer.max_epochs=1500 \
+    data.limit=-1 \
+    data.train_val_test_split="[0.8,0.2,0]" \
+    data.transform="carbune2020_xytn" \
+    model.dropout=0.0,0.25,0.5,0.75 \
+    model.number_of_layers=2,3,4 \
+    model.nodes_per_layer=64 \
+    model.optimizer.lr=0.00005 \
+    model.optimizer.weight_decay=0.0,1e-4 \
+    tags="experiment12" \
+    experiment_name="experiment12" \
+    trial_name="trial2"
