57f4a39e52d664ab8571602f52a4bd5621db2bff
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-05-08 20:19:37 +0100

    Update train_inference_model comments.

--- scripts/train_inference_model.sh
+++ scripts/train_inference_model.sh
@@ -1,13 +1,7 @@
-# 1. Run speed test to see if avoiding re-computing string labels is faster
-#   - Old version to run this script at: a18972f197f8ffae396874b4d7e184919d40b229
-#   - New version to run this script at: 5a056f10c6009de5a34440e4da80a24c9720886a
-# 2. Run speed test to see if removing text decoding, WER/CER computations and logging
-#    thereof is faster
-#   - Old version to run this script at: b464132e91e4f6ac644954347773b7e60754c6e8
-#   - New version to run this script at: e1337475baea1e937237c6d5f4b6b59343e3cf10
+# This script allows training of a model that is used for inference.
 
-# I run those two speed tests using this script here at the two corresponding old and
-# new git commits manually.
+# Hyperparameters are chosen so that they are optimal within the scope
+# of my hyperparameter sweep.
 
 time python src/train.py \
     --config-name experiment_LitModule1.yaml \
