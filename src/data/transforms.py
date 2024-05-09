15c4a022d3f395ea3514c91ef452bc136f694371
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-05-09 18:32:07 +0100

    Update transforms module docstrings and tidy up obsolete comments.

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,16 +1,7 @@
-# Add transforms for datasets here.
+"""Transforms for datasets.
 
-# Datasets will return dicts with keys x, y, (optionally) t, stroke_nr, label, sample_name.
-
-# Transforms that I need for sure:
-# - As input a stack of (x, y, stroke_nr) and as output label.
-# - Switch axis to fit axis of what model reads as input.
-# - same as above but with t
-# - label to alphabet
-# - lower text in label
-# - same as above but with transforms like differences, differences after equidistance transform, Bezier curves
-# - left alone vs lowered label alphabet
-# - TODO: What to code as transform vs what to code as transform?
+Datasets return dicts with keys, e.g. `x`, `y`, `t`, `stroke_nr`, `label`, `sample_name`.
+"""
 
 import torch
 import numpy as np
