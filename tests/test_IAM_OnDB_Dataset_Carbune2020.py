28425add5b1173d2eef1a9ebbcfae3b9f1b56bf0
Martin L (SoPA) <konten.ma.le@gmail.com>
2024-05-17 14:59:25 +0100

    Add models folder.

--- tests/test_IAM_OnDB_Dataset_Carbune2020.py
+++ tests/test_IAM_OnDB_Dataset_Carbune2020.py
@@ -11,4 +11,6 @@ PATH = Path('data/datasets/IAM-OnDB') # Needs to be parameterised
 @pytest.mark.martin
 def test_todo():
 
+    # TODO: WORK ON THIS NEXT!!!
+
     assert 0 == 1, "Add tests! Check if samples are indeed transformed ones. Check for all of the samples!"
